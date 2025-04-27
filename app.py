f6305e3f8536ae87de35b6e8bb65f03bcaf8bfe9
sevinc995 <sevincseferova25@gmail.com>
2025-04-27 03:58:37 +0000

    app py

--- app.py
+++ app.py
@@ -10,3 +10,44 @@ load_dotenv()
 app = Flask(__name__)
 
 CORS(app)
+
+
+def get_db_connection():
+
+    try:
+        conn = psycopg2.connect(
+            host = os.getenv('PGHOST'),
+            USE = OS.getenv('PGUSER'),
+            database = os.getenv('PGDATABASE'),
+            password = os.getenv('PGPASSWORD'),
+            port = os.getenv('PGPORT')
+            sslmode = 'require'
+        )
+    
+        return conn
+
+    except Exception as e:
+        print(f"Database connection error: (e)")
+        return None
+
+@app.route("/api/news", methods=["GET"])
+def get_news():
+
+
+    try:
+        conn = get_db_connection()
+
+        if connection is None:
+            return jsonify({"error": "Database connection failed"}), 500
+
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT id, title, description, image FROM news")
+
+        news = cursor.fetchall()
+
+        cursor.close()
+
+        conn.close()
+
+        
